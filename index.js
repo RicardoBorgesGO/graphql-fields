78326b2264eed144226f0423d616eaa61010ba06
Jose Rocha <tom.jrc@gmail.com>
2017-07-07 14:34:51 +0000

    get metadata

--- index.js
+++ index.js
@@ -43,10 +43,79 @@ function flattenAST(ast, info, obj) {
     }, obj);
 }
 
-module.exports = function graphqlFields(info, obj) {
+/**
+ * Retorna todos os fields de uma consulta utilizando como nome os alias.
+ * @param ast
+ * @param info
+ * @param obj
+ * @returns {*}
+ */
+function flattenASTAlias(ast, info, obj) {
+    obj = obj || {};
+    return getSelections(ast).reduce((flattened, a) => {
+        if (isFragment(a)) {
+            flattened = flattenASTAlias(getAST(a, info), info, flattened);
+        } else {
+            const name = a.alias ? a.alias.value : a.name.value;
+            if (flattened[name]) {
+                Object.assign(flattened[name], flattenASTAlias(a, info, flattened[name]));
+            } else {
+                flattened[name] = flattenASTAlias(a, info);
+            }
+        }
+
+        return flattened;
+    }, obj);
+}
+
+function graphqlFields(info, obj) {
     obj = obj || {};
     const fields = info.fieldNodes || info.fieldASTs;
     return fields.reduce((o, ast) => {
             return flattenAST(ast, info, o);
-    }, obj) || {};
-};
+        }, obj) || {};
+}
+
+function graphqlQuery(ast, typeInfo,visit, visitWithTypeInfo) {
+    let byTypes = [], byFields = [], byFieldsAlias = [], headers = [];
+    visit(ast, visitWithTypeInfo(typeInfo, {
+        Field(node) {
+            const fieldDef = typeInfo.getFieldDef();
+            byTypes.push({
+                name: fieldDef.name,
+                type: fieldDef.type,
+                alias: node.alias ? node.alias.value : "",
+                description: fieldDef.description
+            });
+            !node.selectionSet && (
+                byFields.push(fieldDef.name),
+                    byFieldsAlias.push(node.alias ? node.alias.value : fieldDef.name)
+            );
+        }
+    }));
+    visitNodes(flattenASTAlias(ast.definitions[0]),headers);
+    return {
+        schema: flattenAST(ast.definitions[0]),
+        schemaAlias: flattenASTAlias(ast.definitions[0]),
+        fieldsByNode: headers,
+        fields: byFields,
+        fieldsAlias: byFieldsAlias,
+        mapper: byTypes
+    };
+}
+
+function visitNodes(obj,headers,parent) {
+    for (let key in obj) {
+        if (typeof obj[key] === 'object') {
+            let coluna = parent ? parent + "." + key : key
+            if (Object.keys(obj[key]).length == 0){
+                headers.push(coluna);
+                continue;
+            }
+            visitNodes(obj[key],headers, coluna);
+        }
+    }
+}
+
+
+module.exports = {graphqlFields, flattenAST, flattenASTAlias, graphqlQuery};
